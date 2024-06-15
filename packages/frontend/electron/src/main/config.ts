66154b4f2fc58733b7880e9deaee903a44c5fb6e
yy祝 <62683232+yyznm@users.noreply.github.com>
2024-06-16 02:24:53 +0800

    Update config.ts

--- packages/frontend/electron/src/main/config.ts
+++ packages/frontend/electron/src/main/config.ts
@@ -24,9 +24,9 @@ export const mode = process.env.NODE_ENV;
 export const isDev = mode === 'development';
 
 const API_URL_MAPPING = {
-  stable: `https://app.affine.pro`,
+  stable: `https://n.yyzhu.vip`,
   beta: `https://insider.affine.pro`,
-  canary: `https://affine.fail`,
+  canary: `https://n.yyzhu.vip`,
   internal: `https://insider.affine.pro`,
 };
 
